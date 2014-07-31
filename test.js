182987d1802fc01b54fb87b8af545e94217069bc
Robert Keizer <robert@keizer.ca>
2014-07-30 20:14:50 -0500

    Playing around with scope enumeration still.. basic filter.. horrible effeciency.

--- test.js
+++ test.js
@@ -2,24 +2,48 @@ var util	= require( "util" );
 var Promise	= require( "promise" );
 var async	= require( "async" );
 
-function Checkpointer( checkpointArgs ){
+function Checkpointer( _global, checkpointArgs ){
+
+	var self = this;
+
+	this._filterGlobal = function( scope ){
+		var promise = new Promise( function( resolve, reject ){
+
+			console.log( "I have scope.t of " + scope.t );
+			var _globalKeys = Object.keys( _global );
+			async.filter( Object.keys( scope ), function( key, cb ){
+				if( _globalKeys.indexOf( key ) >= 0 ){
+					return cb( false );
+				}
+				return cb( true );
+			}, function( res ){
+				return resolve( res );
+			} );
+		} );
+
+		return promise;
+	};
 	
 	this.checkpoint = function( scope ){
 		var promise = new Promise( function( resolve, reject ){
-			console.log( "Would look through scope.." );
-			return resolve( null );
+			self._filterGlobal( scope ).then( function( filteredScope ){
+				console.log( "I have filteredScope of " + JSON.stringify( filteredScope ) );
+				return resolve( null );
+			} );
 		} );
 		return promise;
 	};
 };
 
-var myCheckpointer = new Checkpointer( { "foo": "bar" } );
+var myCheckpointer = new Checkpointer( this, { "foo": "bar" } );
 
 console.log( util.inspect( myCheckpointer ) );
 
 var testFunc = function( argA, argB, cb ){
 	
 	var t = argA + argB;
+
+	console.log( "this.t is " + this.t + " ( in testFunc )" );
 
 	myCheckpointer.checkpoint( this ).then( function( ){
 		cb( t % argA );
